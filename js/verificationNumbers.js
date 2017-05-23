95f7458e21195d316beea0d271d2f6ec2fd28788
liuqian0413 <784575932@qq.com>
2017-05-23 14:16:43 +0800

    '动态粒子登录界面提交17/5/23'

---
+++ js/verificationNumbers.js
@@ -0,0 +1,9 @@
+function showCheck(a){
+	var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+	ctx.clearRect(0,0,1000,1000);
+	ctx.font = "80px 'Microsoft Yahei'";
+	ctx.fillText(a,0,100);
+	ctx.fillStyle = "white";
+}
+
